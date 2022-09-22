305b3396f7918e33e0e7140dbb34ef260bdf7b8c
ak-umka <umbet70@gmail.com>
2022-09-22 20:15:34 +0600

    auth on back

---
+++ back/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import router from './router/auth-route.js';
+import errorMiddleware from './middlewares/error-middlewares.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/v0', router);
+app.use(errorMiddleware);
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }, () => { console.log('Connected to DB!') });
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
