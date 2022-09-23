c2fdcebccf3b5cd9465c25b576d6ada2e3575061
ak-umka <umbet70@gmail.com>
2022-09-24 04:29:14 +0600

    fix comment

--- back/models/rating-model.js
+++ back/models/rating-model.js
@@ -3,6 +3,11 @@ import mongoose from "mongoose";
 const ratingSchema = new mongoose.Schema({
   rating: {
     type: Number,
+    min: 1,
+    max: 5,
+    validate: {
+      validator: Number.isInteger,
+    },
   },
   comment: {
     type: String,
