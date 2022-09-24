b80261942694ea6fca5579f0fe8c8b92a99df6cb
zhazira oraz <zhaziraoraz@MacBook-Air-zhazira.local>
2022-09-24 12:13:33 +0600

    redirect

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -1,15 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-// import About from './components/About'
 import Home from '../components/Home'
-// import Users from './components/Users'
+import PrivateRoute from './privateRoute'
+import { useSelector } from 'react-redux'
+import { RootState } from '../store'
+import Create from '../components/Create'
+import SignIn from '../components/SignIn'
 
 const MyRoutes = () => {
+  const isAuthenticated = useSelector((state: RootState) => state.auth)
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route
+            path="/create"
+            element={
+              <PrivateRoute isAuthenticated={isAuthenticated.loggedIn}>
+                <Create />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
