b9bd62f4e59bf3b019bcbef59a244aa0386d6e12
zhazira oraz <zhaziraoraz@MacBook-Air-zhazira.local>
2022-09-24 14:21:26 +0600

    schoolInformation

--- front/src/components/SignIn/index.tsx
+++ front/src/components/SignIn/index.tsx
@@ -1,8 +1,31 @@
-function SignIn() {
+import { useSelector, useDispatch, connect } from 'react-redux'
+import { useEffect } from 'react'
+import { getSchools } from '../../store/action/schoolAction'
+import { bindActionCreators, Dispatch } from 'redux'
+import { Types } from '../../store/types'
+
+type Props = ReturnType<typeof mapDispatchToProps>
+
+function SignIn(props: Props) {
+  const dispatch = useDispatch()
+
+  // useEffect(() => {
+  //   props.getSchools()
+  // }, [])
+
   return (
     <div>
       <h1>Sign in</h1>
     </div>
+  )
+}
+
+const mapDispatchToProps = (dispatch: Dispatch<Types>) => {
+  return bindActionCreators<any, any>(
+    {
+      getSchools,
+    },
+    dispatch,
   )
 }
 
